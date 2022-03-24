cac83b9e32612f98c31e75eafffa017c993e3a6c
Saif Abrar <62766337+saifabrar17@users.noreply.github.com>
2022-03-24 22:27:22 +0600

    header complete

---
+++ src/components/Shop/Shop.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Shop.css'
+
+const Shop = () => {
+    return (
+        <div className='shop-container'>
+            <div className="products-container">
+                <p>this is for product</p>
+            </div>
+            <div className="cart-container">
+                <p>order summary</p>
+            </div>
+        </div>
+    );
+};
+
+export default Shop;
