d982345febcae94cd0723e8854e4c42d68741cce
Saif Abrar <62766337+saifabrar17@users.noreply.github.com>
2022-03-25 14:24:11 +0600

    working with event handling

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -2,11 +2,24 @@ import React from 'react';
 import './product.css';
 
 const Product = (props) => {
-    const {name, price, seller, img, ratings} = props.product;
+    const { name, price, seller, img, ratings } = props.product;
+
+    const handleAddToCart = () => {
+        console.log('clicked');
+    }
     return (
-        <div>
+        <div className='product-card'>
             <img src={img} className='product-img' alt="" />
-            <h2>{name}</h2>
+
+            <div className="product-info">
+                <h2 className='product-name'>{name}</h2>
+                <p>Price: ${price}</p>
+                <p><small>Manufacturer: {seller}</small></p>
+                <p><small>Ratings: {ratings} stars</small></p>
+            </div>
+            <button onClick={handleAddToCart} className='btn-cart'>
+                <p>Add to Cart</p>
+            </button>
         </div>
     );
 };
